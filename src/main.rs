2571accf757ab862e71e9a4363f46c346d906385
pichst123 <hoangpichgoodkid@gmail.com>
2024-01-15 23:53:59 +0700

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 slint::include_modules!();
 
+
+
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
 
     let ui_handle = ui.as_weak();
-    // ui.on_request_increase_value(move || {
-    //     let ui = ui_handle.unwrap();
-    //     ui.set_counter(ui.get_counter() + 1);
-    // });
+    ui.on_divide_income(move |string| {
+        let ui = ui_handle.unwrap();
+        let mess: String = string.trim().parse().unwrap();
+ 
+        let result = format!("Messenge", mess);
+        ui.set_results(result.into());
+    });
 
     ui.run()
 }
