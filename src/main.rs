42fed93747e557be7050dfaba60654fe5db7ac3d
pichtran2004 <126134422+pichtranst123@users.noreply.github.com>
2024-01-16 00:00:13 +0700

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,13 @@
 slint::include_modules!();
 
-
-
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
 
     let ui_handle = ui.as_weak();
-    ui.on_divide_income(move |string| {
+    ui.on_divide_income(move |name, message| {
         let ui = ui_handle.unwrap();
-        let mess: String = string.trim().parse().unwrap();
- 
-        let result = format!("Messenge", mess);
-        ui.set_results(result.into());
+        let formatted_message = format!("{}: {}", name.trim(), message.trim());
+        ui.set_results(formatted_message.into());
     });
 
     ui.run()
